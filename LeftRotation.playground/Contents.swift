88555cd5238112a555fcf82e40c57f62140e6c1a
jatac <jan.atac@vialink.fr>
2017-08-23 13:33:20 +0200

    Refactored

--- LeftRotation.playground/Contents.swift
+++ LeftRotation.playground/Contents.swift
@@ -1,43 +1,50 @@
 import Foundation
 
-var d = 5
-var inputTmp = "5 4"
-var input = "1 2 3 4 5"
+var nb = "20 10"
+var input = "41 73 89 7 10 1 59 58 84 77 77 97 58 1 86 58 26 10 86 51"
 ////////////////////////////////
 
-var array:[String] = []
-var str = ""
+leftRotate(nb: nb, input: input)
 
-
-
-for a in input.characters {
-    if a != " " {
-        str.append(a)
-    } else {
-        array.append(str)
-        str = ""
+func leftRotate(nb:String, input:String){
+    
+    var d:Int!  = Int(getArrayFromString(str: nb).last!)
+    var arr     = getArrayFromString(str: input)
+    
+    var n       = arr.count
+    
+    if n >= 1 && n <= 100000 {
+        if d >= 1 && d <= n {
+            for i in 0..<d {
+                var y = arr[0]
+                arr.remove(at: 0)
+                arr.append(y)
+            }
+            
+            var output = ""
+            
+            for x in arr {
+                output.append(x)
+                output.append(" ")
+            }
+            
+            print("\(output)")
+        }
     }
+
 }
 
-array.append(str)
-
-var n = array.count
-
-if n >= 1 && n <= 100000 {
-    if d >= 1 && d <= n {
-        for i in 0..<d {
-            var y = array[0]
-            array.remove(at: 0)
-            array.append(y)
+func getArrayFromString(str:String)->[String]{
+    var arrayTmp:[String] = []
+    var strTmp = ""
+    for x in str.characters {
+        if x != " " {
+            strTmp.append(x)
+        } else {
+            arrayTmp.append(strTmp)
+            strTmp = ""
         }
-        
-        var output = ""
-        
-        for x in array {
-            output.append(x)
-            output.append(" ")
-        }
-        
-        print("\(output)")
     }
+    arrayTmp.append(strTmp)
+    return arrayTmp
 }
