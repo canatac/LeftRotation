3ebe54691f9dbd89e6f726a559de5f20737a1cb3
jatac <jan.atac@vialink.fr>
2017-08-23 13:02:52 +0200

    added constraints

--- LeftRotation.playground/Contents.swift
+++ LeftRotation.playground/Contents.swift
@@ -1,13 +1,15 @@
 import Foundation
 
-var d = 4
-var n = "5 4"
+var d = 5
+var inputTmp = "5 4"
 var input = "1 2 3 4 5"
-
 ////////////////////////////////
 
 var array:[String] = []
 var str = ""
+
+
+
 for a in input.characters {
     if a != " " {
         str.append(a)
@@ -19,18 +21,23 @@ for a in input.characters {
 
 array.append(str)
 
-for i in 0..<d {
-    var y = array[0]
-    array.remove(at: 0)
-    array.append(y)
+var n = array.count
+
+if n >= 1 && n <= 100000 {
+    if d >= 1 && d <= n {
+        for i in 0..<d {
+            var y = array[0]
+            array.remove(at: 0)
+            array.append(y)
+        }
+        
+        var output = ""
+        
+        for x in array {
+            output.append(x)
+            output.append(" ")
+        }
+        
+        print("\(output)")
+    }
 }
-
-var output = ""
-
-for x in array {
-    output.append(x)
-    output.append(" ")
-}
-
-print("\(output)")
-
